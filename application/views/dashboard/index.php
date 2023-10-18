ba7f09b1edd275fcf0ca2d0be44c05d078a98c0f
IntoYourdreamS <DemonBubble24@gmail.com>
2023-10-18 09:02:30 +0700

    ngubah base_url

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -19,7 +19,7 @@
 
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__wobble" src="<?= base_url('assets/');?>dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">echo "# ProjekTugas" >> README.md
+    <img class="animation__wobble" src="<?= base_url('assets/');?>dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
   </div>
 
   <!-- Navbar -->
