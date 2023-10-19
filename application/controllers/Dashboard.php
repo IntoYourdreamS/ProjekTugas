39965b8bb2765e4f05e6e708571f31cc93b28610
abdil7 <achmadabdil321@gmail.com>
2023-10-19 07:40:22 +0700

    cuy

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -4,6 +4,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Dashboard extends CI_Controller {
 	public function index()
 	{
+		$data['title'] = 'Dashboard';
+		
+		$this->load->view('templates/header', $data);
+		$this->load->view('templates/sidebar');
 		$this->load->view('dashboard/index');
+		$this->load->view('templates/footer');
 	}
 }
---
+++ application/views/templates/footer.php
@@ -0,0 +1,29 @@
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.2.0
+    </div>
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+<!-- jQuery -->
+<script src="<?= base_url('assets/');?>plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="<?= base_url('assets/');?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="<?= base_url('assets/');?>plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?= base_url('assets/');?>dist/js/adminlte.js"></script>
+</body>
+</html>
